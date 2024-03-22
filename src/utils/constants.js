c23e1df14dfa65b9156452ac5068259e5b1e1a7b
Ravi verma <vermaravi8809@gmail.com>
2024-03-23 02:47:27 +0530

    fixing api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@
 // export const API_BASE_URL = "http://localhost:9002/api";
 
 // FOR PRODUCTION
-export const API_BASE_URL = "https://blog-backend-et1w.onrender.com";
+export const API_BASE_URL = "https://blog-backend-et1w.onrender.com/api";
